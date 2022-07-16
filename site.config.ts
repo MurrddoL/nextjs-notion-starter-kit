7ea85a2f98f8d0bfc4d434eb2520d918dfd3f83b
MurrddoL <murrddo@hotmail.com>
2022-07-16 14:40:41 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '7fc01fbf90a048f8ab06fa1bf444cf68',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'MurrddoL's Bolg',
+  domain: 'blog.murrddol.com',
+  author: 'Murrddo Lin',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
